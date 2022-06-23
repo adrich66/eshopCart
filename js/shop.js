d3ddc7eed6280f34f0f6c0c257a3779cd8c39197
Adrià Moya Cangueiro <adri@MacBook-Air-de-Adria.local>
2022-06-23 12:18:40 +0200

    ejercicio 5 y crear products.js a parte

--- js/shop.js
+++ js/shop.js
@@ -1,89 +1,22 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-// const products = JSON.stringify('./products.json'); ???
+//Done it, creando un archivo js con los products y llamandoles antes de shop.js en los html
 
-var products = [
-  {
-    id: 1,
-    name: "cooking oil",
-    price: 10.5,
-    type: "grocery",
-    offer: {
-      number: 3,
-      percent: 20,
-    },
-  },
-  {
-    id: 2,
-    name: "Pasta",
-    price: 6.25,
-    type: "grocery",
-  },
-  {
-    id: 3,
-    name: "Instant cupcake mixture",
-    price: 5,
-    type: "grocery",
-    offer: {
-      number: 10,
-      percent: 30,
-    },
-  },
-  {
-    id: 4,
-    name: "All-in-one",
-    price: 260,
-    type: "beauty",
-  },
-  {
-    id: 5,
-    name: "Zero Make-up Kit",
-    price: 20.5,
-    type: "beauty",
-  },
-  {
-    id: 6,
-    name: "Lip Tints",
-    price: 12.75,
-    type: "beauty",
-  },
-  {
-    id: 7,
-    name: "Lawn Dress",
-    price: 15,
-    type: "clothes",
-  },
-  {
-    id: 8,
-    name: "Lawn-Chiffon Combo",
-    price: 19.99,
-    type: "clothes",
-  },
-  {
-    id: 9,
-    name: "Toddler Frock",
-    price: 9.99,
-    type: "clothes",
-  },
-];
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
-
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
-
 var total = 0;
 
 // Exercise 1
 function buy(id) {
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
   for (let i = 0; i < products.length; i++) {
     if (products[i].id === id) {
       cartList.push(products[i]);
     }
   }
   console.log(cartList);
-  
-  // 1. Loop for to the array products to get the item to add to cart
-  // 2. Add found product to the cartList array
 }
 
 // Exercise 2
@@ -93,6 +26,7 @@ function cleanCart() {
 
 // Exercise 3
 function calculateTotal() {
+  // Calculate total price of the cart using the "cartList" array
   let totalPrice = 0;
 
   for (let i = 0; i < cartList.length; i++) {
@@ -100,12 +34,15 @@ function calculateTotal() {
   }
 
   return totalPrice;
-  // Calculate total price of the cart using the "cartList" array
+
 }
 
 // Exercise 4
 function generateCart() {
-
+  /* Using the "cartlist" array that contains all the items in the shopping cart, 
+    generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    button refresh was created in cart in modal fade */
+  cart = [];
   // Para cada objeto del array cartList lo recorro con for
   for (let i = 0; i < cartList.length; i++) {
     // Primer caso excepcionalmente para llenar el array cart
@@ -113,6 +50,7 @@ function generateCart() {
       cart.push(cartList[i]);
       cart[i].quantity = 1;
       cart[i].subtotal = cart[i].price*cart[i].quantity;
+      cart[i].subtotalWithDiscount = cart[i].subtotal;
     //después de llenar con uno siempre pasará al siguiente else
     }else{
       let j = 0
@@ -123,6 +61,7 @@ function generateCart() {
         if(cart[j].id === cartList[i].id){
           cart[j].quantity++;
           cart[j].subtotal = cart[j].price*cart[j].quantity;
+          cart[j].subtotalWithDiscount = cart[j].subtotal;
           condition = true;
         }
         j++;
@@ -131,20 +70,34 @@ function generateCart() {
       if (!condition){
         cart.push({...cartList[i]});
         cart[cart.length -1].quantity = 1;
+        cart[cart.length -1].subtotal = cart[cart.length-1].price;
+        cart[cart.length -1].subtotalWithDiscount = cart[cart.length-1].subtotal;
       }
 
     }
   }
-
+  applyPromotionsCart();
   console.log(cart);
-  /* Using the "cartlist" array that contains all the items in the shopping cart, 
-    generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    button refresh was created in cart in modal fade */
 }
 
 // Exercise 5
 function applyPromotionsCart() {
   // Apply promotions to each item in the array "cart"
+
+  for (let i = 0; i < cart.length; i++) {
+      if(
+        (cart[i].id === 1) 
+        && (cart[i].quantity >= 3)) {
+          cart[i].subtotalWithDiscount = 10 * (cart[i].quantity);
+        }
+      if(
+        (cart[i].id === 3) 
+        && (cart[i].quantity >= 10)) {
+          cart[i].subtotalWithDiscount = (((cart[i].subtotal) / 3 )* 2).toFixed(2);
+        }
+  }
+
+  
 }
 
 // Exercise 6
@@ -169,6 +122,5 @@ function removeFromCart(id) {
 
 function open_modal() {
   console.log("Open Modal");
-  printCart();
-  generateCart();
+  printCart(); 
 }
