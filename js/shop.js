ae3afb9b2e5e047d3a6cd6bf7cc9f7c105256b77
Adrià Moya Cangueiro <adri@MacBook-Air-de-Adria.local>
2022-06-23 09:59:54 +0200

    ejercicio 4

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,70 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
+// const products = JSON.stringify('./products.json'); ???
+
 var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
+  {
+    id: 1,
+    name: "cooking oil",
+    price: 10.5,
+    type: "grocery",
+    offer: {
+      number: 3,
+      percent: 20,
     },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
+  },
+  {
+    id: 2,
+    name: "Pasta",
+    price: 6.25,
+    type: "grocery",
+  },
+  {
+    id: 3,
+    name: "Instant cupcake mixture",
+    price: 5,
+    type: "grocery",
+    offer: {
+      number: 10,
+      percent: 30,
     },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+  },
+  {
+    id: 4,
+    name: "All-in-one",
+    price: 260,
+    type: "beauty",
+  },
+  {
+    id: 5,
+    name: "Zero Make-up Kit",
+    price: 20.5,
+    type: "beauty",
+  },
+  {
+    id: 6,
+    name: "Lip Tints",
+    price: 12.75,
+    type: "beauty",
+  },
+  {
+    id: 7,
+    name: "Lawn Dress",
+    price: 15,
+    type: "clothes",
+  },
+  {
+    id: 8,
+    name: "Lawn-Chiffon Combo",
+    price: 19.99,
+    type: "clothes",
+  },
+  {
+    id: 9,
+    name: "Toddler Frock",
+    price: 9.99,
+    type: "clothes",
+  },
+];
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
 
@@ -73,74 +75,100 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-
-    for(let i = 0; i < products.length; i++){
-
-        if ( products[i].id == id){
-            cartList.push(products[i]);
-        }
-    };
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      cartList.push(products[i]);
+    }
+  }
+  console.log(cartList);
+  
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
 }
 
 // Exercise 2
 function cleanCart() {
-    cartList = [];
+  cartList = [];
 }
 
 // Exercise 3
 function calculateTotal() {
-let totalPrice = 0;
+  let totalPrice = 0;
 
-    for (let i = 0; i < cartList.length; i++){
-        totalPrice += cartList[i].price;
-    }
+  for (let i = 0; i < cartList.length; i++) {
+    totalPrice += cartList[i].price;
+  }
 
-return totalPrice;
-    // Calculate total price of the cart using the "cartList" array
+  return totalPrice;
+  // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
 function generateCart() {
 
+  // Para cada objeto del array cartList lo recorro con for
+  for (let i = 0; i < cartList.length; i++) {
+    // Primer caso excepcionalmente para llenar el array cart
+    if(cart.length === 0){
+      cart.push(cartList[i]);
+      cart[i].quantity = 1;
+      cart[i].subtotal = cart[i].price*cart[i].quantity;
+    //después de llenar con uno siempre pasará al siguiente else
+    }else{
+      let j = 0
+      let condition = false;
+      // Lo busco en el array con while dandole dos condiciones no como for
+      while (j < cart.length && condition === false){
+        // si lo encuentro!! -> actualizo valores y le doy condition true para el siguiente paso
+        if(cart[j].id === cartList[i].id){
+          cart[j].quantity++;
+          cart[j].subtotal = cart[j].price*cart[j].quantity;
+          condition = true;
+        }
+        j++;
+      }
+      //si no lo encuentro!! -> lo creo duplicandolo del cartList y añadiendo quantity
+      if (!condition){
+        cart.push({...cartList[i]});
+        cart[cart.length -1].quantity = 1;
+      }
 
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    }
+  }
+
+  console.log(cart);
+  /* Using the "cartlist" array that contains all the items in the shopping cart, 
+    generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    button refresh was created in cart in modal fade */
 }
 
 // Exercise 5
 function applyPromotionsCart() {
-
-
-    // Apply promotions to each item in the array "cart"
+  // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 6
 function printCart() {
-
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  // Fill the shopping cart modal manipulating the shopping cart dom
 }
 
 // Exercise 7
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  // Refactor previous code in order to simplify it
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
 // ** Nivell II **
 
-
 // Exercise 8
 function removeFromCart(id) {
-
-
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+  console.log("Open Modal");
+  printCart();
+  generateCart();
 }
